d1518d678a3241cb228de58be8946e8ff9ea199b
Andrew Ferguson <andrew@fergcorp.com>
2016-07-09 18:34:50 -0700

    Add quicktag to make adding passages easier

--- fergcorp_esv.php
+++ fergcorp_esv.php
@@ -36,6 +36,7 @@ Class Fergcorp_ESV {
 	 */
 	public function __construct(){
 		add_shortcode('esv', array ( &$this, 'shortcode_esv' ) );
+		add_action( 'admin_print_footer_scripts', array(&$this, 'add_quicktags' ));
 	}
 
 	/**
@@ -100,6 +101,71 @@ Class Fergcorp_ESV {
 		return $body;
 		
 	}
+	
+	// add more buttons to the html editor
+	function add_quicktags() {
+	    if (wp_script_is('quicktags')){
+	?>
+	    <script type="text/javascript">
+		// http://wordpress.stackexchange.com/questions/41422/functional-quicktag
+		// https://gist.github.com/zoerooney/755226cc8271f468037e
+		// Add a button called 'abbr' with a callback function
+		    QTags.addButton( 'esv', 'ESV', esv_prompt );
+		    // and this is the callback function
+		    function esv_prompt(e, c, ed) {
+		        var prmt, t = this;
+		        if ( ed.canvas.selectionStart !== ed.canvas.selectionEnd ) {
+		            // if we have a selection in the editor define out tagStart and tagEnd to wrap around the text
+		            // prompt the user for the abbreviation and return gracefully on a null input
+		            selection = ed.canvas.value.substring  (ed.canvas.selectionStart, ed.canvas.selectionEnd);
+		            if ( selection === null ) return;
+		            t.tagStart = '[esv passage="';
+		            t.tagEnd = '"]';
+		        } else if ( ed.openTags ) {
+		            // if we have an open tag, see if it's ours
+		            var ret = false, i = 0, t = this;
+		            while ( i < ed.openTags.length ) {
+		                ret = ed.openTags[i] == t.id ? i : false;
+		                i ++;
+		            }
+		            if ( ret === false ) {
+		                // if the open tags don't include 'abbr' prompt for input
+		                prmt = prompt('Enter Passage (e.g. Luke 1:15-20)');
+		                if ( prmt === null ) return;
+		            	t.tagStart = '[esv passage="' + prmt + '"]';
+		                t.tagEnd = false;
+		                if ( ! ed.openTags ) {
+		                    ed.openTags = [];
+		                }
+		                ed.openTags.push(t.id);
+		                e.value = '/' + e.value;
+		            } else {
+		                // otherwise close the 'abbr' tag
+		                ed.openTags.splice(ret, 1);
+		                t.tagStart = '</abbr>';
+		                e.value = t.display;
+		            }
+		        } else {
+		            // last resort, no selection and no open tags
+		            // so prompt for input and just open the tag
+		            prmt = prompt('Enter Passage (e.g. Luke 1:15-20)');
+		            if ( prmt === null ) return;
+	            	t.tagStart = '[esv passage="' + prmt + '"]';
+	                t.tagEnd = false;
+		            if ( ! ed.openTags ) {
+		                ed.openTags = [];
+		            }
+		            ed.openTags.push(t.id);
+		            e.value = '/' + e.value;
+		        }
+		        // now we've defined all the tagStart, tagEnd and openTags we process it all to the active window
+		        QTags.TagButton.prototype.callback.call(t, e, c, ed);
+		    };
+		</script>
+	<?php
+	    }
+	}
+	
 	
 	/*
 	protected function getBiblePassage ($query_string = '', $error_message = 'ERROR: Could not retrieve readings') {
